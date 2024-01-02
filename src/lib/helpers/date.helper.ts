b5f5b1b53eeec51547f57570818ee540beb946e1
Brian Schwabauer <brian@brianschwabauer.com>
2024-01-02 06:30:00 -0600

    Add getUTCDate Helper

--- src/lib/helpers/date.helper.ts
+++ src/lib/helpers/date.helper.ts
@@ -8,3 +8,17 @@ export function getFirstDayOfWeek(
 		parsed.getUTCDate() - ((parsed.getUTCDay() - (startWeekOnSunday ? 0 : 1) + 7) % 7),
 	);
 }
+
+/** Returns a date object in UTC time for the given date (or year/month/day) */
+export function getUTCDate(year: number, month?: number, day?: number): Date;
+export function getUTCDate(date: Date | string): Date;
+export function getUTCDate(
+	date: Date | number | string,
+	maybeMonth?: number,
+	maybeDay?: number,
+) {
+	if (typeof date === 'number') {
+		return new Date(Date.UTC(date, maybeMonth || 0, maybeDay ?? 1));
+	}
+	return new Date(new Date(date).setUTCHours(0, 0, 0, 0));
+}
