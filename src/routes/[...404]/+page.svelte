4f9a73f5009c4b84ad1e5c053a34346ece2cbc81
Brian Schwabauer <brian@brianschwabauer.com>
2024-01-03 16:04:25 -0600

    Add 404 page

---
+++ src/routes/[...404]/+page.svelte
@@ -0,0 +1,22 @@
+
+<main>
+	<h1>Page Not Found</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+	}
+	h1 {
+		font-size: 3rem;
+		font-weight: bold;
+		margin: 0;
+		padding: 0;
+		line-height: 3rem;
+		color: white;
+	}
+</style>
